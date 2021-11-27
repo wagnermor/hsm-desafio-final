6737cc8b6f4560512b0702f54e1a3214c0bb3486
WagnerMor <wagnermor@outlook.com>
2021-11-27 20:47:38 -0300

    fix: only html, css and js files

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const btn_escolher = document.getElementById("btn_escolher");
+const btn_concluir = document.getElementById("btn_concluir");
+
+const actions = document.getElementById("actions");
+const id = document.getElementById("id");
+const titulo = document.getElementById("titulo");
+const descricao = document.getElementById("descricao");
+const link_imagem = document.getElementById("link_imagem");
+const nome_promo = document.getElementById("nome_promo");
+const lista = document.getElementById("lista");
+
